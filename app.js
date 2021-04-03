fccd91cab6a8e186941bf046744f22cba8448794
alirezadalvand <alireza.d1339@gmail.com>
2021-04-03 12:30:28 +0430

    files added

---
+++ app.js
@@ -0,0 +1,13 @@
+// const par = "bahar love Hot chips with Chili soS";
+
+// const words = par.split("o")
+//  console.log(words[2]);
+
+const num = 5;
+// console.log(num);
+
+let bahar = num.toString();
+// console.log(bahar);
+
+const sum = num + bahar;
+console.log(sum);
